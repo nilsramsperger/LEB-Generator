a7ac72d33b11f9545602873e6f480ad75b3123c2
nils <nils@ramsperger-home.de>
2019-05-05 19:27:14 +0200

    Kompilate entfernt.

--- src/businessLogic/LebGenerator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var LebGenerator = /** @class */ (function () {
-    function LebGenerator() {
-    }
-    return LebGenerator;
-}());
-exports.LebGenerator = LebGenerator;
-//# sourceMappingURL=LebGenerator.js.map
